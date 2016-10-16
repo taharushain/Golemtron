20018270bdb8f6f5eedca56b72c607be5a3c4bf0
Taha Rushain <taharushain@gmail.com>
2016-10-17 00:24:05 +0500

    Fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,6 +60,17 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.perform_caching = false
+  config.action_mailer.smtp_settings = {
+    :address => "email-smtp.us-west-2.amazonaws.com",
+    :port => 587,
+  :user_name => ENV["SES_SMTP_USERNAME"], #Your SMTP user
+  :password => ENV["SES_SMTP_PASSWORD"], #Your SMTP password
+  :authentication => :login,
+  :enable_starttls_auto => true
+}
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
