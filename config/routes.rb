e90930b63bae10bb506b163a2d367d4016021376
Taha Rushain <taharushain@gmail.com>
2016-10-17 21:42:02 +0500

    Begin - Blog

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  mount Spina::Engine => '/'
-	resources :messages, only: [:new, :create]
 	root 'welcome#home'
 	get "/about" => "welcome#about"
 	get "/packages" => "welcome#packages"
 	get "/contact" => "welcome#contact"
 	get "/blog" => "blog#home"
-	match '/send_order', to: 'welcome#send_order', via: 'post'
+	# match '/send_order', to: 'welcome#send_order', via: 'post'
 
 end
